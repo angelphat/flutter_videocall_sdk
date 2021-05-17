064b579209e388e37b97dd16dc6471161ccf9066
phatangel <phatkhatuan@gmail.com>
2021-05-17 15:43:08 +0700

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [0.0.2] - TODO: Add release date.
+## [0.0.3] - TODO: Add release date.
 
     * An easy way to build a app video call
--- example/videotest.dart
+++ example/videotest.dart
@@ -10,61 +10,64 @@ class VideoTest extends StatefulWidget{
 }
 
 class _VideoTestState extends State<VideoTest>{
-  String _address;
+  String _address = "";
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Video Call Example Test'),
-      ),
-      body: Container(
-        margin: const EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.connect_without_contact),
-                hintText: 'Type address of your server',
-                labelText: 'Server Address',
-              ),
-              onFieldSubmitted: (String textInput){
-                  _address = textInput;
-              },
-              validator:(String value){
-                return (value != null && value.isEmpty) ? 'Please input your server Address' : null;
-              },
-            ),
-            TextButton(
-                style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                  overlayColor: MaterialStateProperty.resolveWith<Color>(
-                        (Set<MaterialState> states) {
-                      if (states.contains(MaterialState.hovered))
-                        return Colors.blue.withOpacity(0.02);
-                      if (states.contains(MaterialState.focused) ||
-                          states.contains(MaterialState.pressed))
-                        return Colors.blueGrey;
-                      return null; // Defer to the widget's default.
-                    },
-                  ),
-                ),
-                onPressed: () {
-                  if(_address.isEmpty){
-                    return;
-                  }
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => FlutterVideoCall(serverAddress: _address)));
-                },
-                child: Text('Connect')
-            )
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Video Call Example Test'),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body:
+        Container(
+          margin: const EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.connect_without_contact),
+                  hintText: 'Type address of your server',
+                  labelText: 'Server Address',
+                ),
+                onChanged: (String textInput){
+                    _address = textInput;
+                },
+                validator:(String value){
+                  return (value != null && value.isEmpty) ? 'Please input your server Address' : null;
+                },
+              ),
+              TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                    overlayColor: MaterialStateProperty.resolveWith<Color>(
+                          (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.hovered))
+                          return Colors.blue.withOpacity(0.02);
+                        if (states.contains(MaterialState.focused) ||
+                            states.contains(MaterialState.pressed))
+                          return Colors.blueGrey;
+                        return null; // Defer to the widget's default.
+                      },
+                    ),
+                  ),
+                  onPressed: () {
+                    if(_address.isEmpty){
+                      return;
+                    }
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => FlutterVideoCall(serverAddress: _address)));
+                  },
+                  child: Text('Connect')
+              )
+            ],
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
